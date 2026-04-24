b5594dc7f0003a0277ee547f1b7808b1e648ccbf
Elina Reznichenko <missflash96@gmail.com>
2026-04-24 16:48:12 +0300

    fix Notes client filename

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,7 @@
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 import { getQueryClient } from "@/lib/query-client";
-import NotesClient from "./notes.client";
+import NotesClient from "./Notes.client";
 
 interface NotesPageProps {
   searchParams: Promise<{
